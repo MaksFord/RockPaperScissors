72935d9752d29378661bcffec1631ae9e2da9dff
MaksFord <118970421+MaksFord@users.noreply.github.com>
2023-02-15 23:12:24 +0600

    Graphical interface added
    Still need some improvement

--- RPS.js
+++ RPS.js
@@ -3,7 +3,14 @@ for (let i=0; i<5; i++){
     buttons[i].addEventListener("click", event=>whoWon(i));
 }   
 }
-const pcChoice=document.getElementById("pcChoice");
+
+const pcImage=document.getElementById("pcImage");
+var cj=0;
+var switchImage=setInterval(function(){
+    pcImage.innerHTML='<img src="'+optionImages[cj]+'" width=100px>';
+    cj=(cj+1)%optionImages.length;
+},300);
+
 gameStates=[
     [0, 1, -1, -1, 1],
     [-1, 0, 1, 1, -1],
@@ -12,10 +19,13 @@ gameStates=[
     [-1, 1, 1, -1, 0]
 ]
 optionNames=["Rock","Paper","Scissors","Lizard","Spock"]
+optionImages=["Rock.jpg","Paper.jpg","Scissors.jpg","Lizard.jpg","Spock.jpg"]
 
 async function whoWon (i) {
+clearInterval(switchImage)
 let j=Math.floor(Math.random()*5);
-pcChoice.innerHTML=`${optionNames[j]}`;
+
+pcImage.innerHTML='<img src="'+optionImages[j]+'" width=100px>';
 await delay(1000);
 
 gameState=gameStates[j][i]
