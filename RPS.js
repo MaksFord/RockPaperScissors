62c4b06792bdc82b075a221fbb892e47612e1646
MaksFord <118970421+MaksFord@users.noreply.github.com>
2023-02-16 18:39:00 +0600

    Scores added
    alerts rewritten

--- RPS.js
+++ RPS.js
@@ -6,12 +6,12 @@ document.getElementById("plus").addEventListener("click", event=>gamePlus())
 }
 
 const pcImage=document.getElementById("pcImage");
-var cj=0;
+let cj=0;
 var switchImage=setInterval(function(){
     pcImage.innerHTML='<img src="'+optionImages[cj]+'" width=100px>';
     cj=(cj+1)%optionImages.length;
 },300);
-var n=3;
+let n=3;
 gameStates=[
     [0, 1, -1, -1, 1],
     [-1, 0, 1, 1, -1],
@@ -32,20 +32,36 @@ function gamePlus (){
     hiddens.forEach((hidden)=>{hidden.classList.remove("secondary")});
     return n;
 }
+const current=document.getElementById("current");
+const scores=document.getElementById("scores");
+let playerScore=0;
+let computerScore=0;
+updateScores=()=>scores.innerHTML=`Player score: ${playerScore} <br>Computer score: ${computerScore}`; 
+
 
 async function whoWon (i,n) {
 clearInterval(switchImage)
 let j=Math.floor(Math.random()*n);
 
 pcImage.innerHTML='<img src="'+optionImages[j]+'" width=100px>';
-await delay(1000);
+await delay(500);
 
 gameState=gameStates[j][i]
 if (gameState===1)
-{alert ("player won! "+optionNames[i]+" beats "+optionNames[j])}
+{current.innerHTML="Player won! "+optionNames[i]+" beats "+optionNames[j];
+playerScore++; 
+updateScores()
+return playerScore;
+
+}
 else if (gameState===-1)
-{alert ("computer won! "+optionNames[j]+" beats "+optionNames[i])}
-else{alert ("tie! "+optionNames[i]+" = "+optionNames[j])}
+{current.innerHTML="Computer won! "+optionNames[j]+" beats "+optionNames[i]
+computerScore++;
+updateScores()
+return computerScore;}
+else{current.innerHTML="tie! "+optionNames[i]+" = "+optionNames[j]}
 }
 //pause function
 const delay = ms => new Promise(res => setTimeout(res, ms));
+
+
